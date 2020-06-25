5ff2c7e80af1fde6872af51fd21f188a815e1595
KleberNeves <kleber.na@gmail.com>
2020-06-25 10:47:55 -0300

    separated evaluation and replication functions from model.r

---
+++ eval.r
@@ -0,0 +1,478 @@
+# Measures to evaluate the corpus of estimates of effect sizes
+# General function that calculates all rates, in all cases
+make.evaluation.tests = function(input) {
+  df = data.frame()
+  
+  t = "Error rates and discoveries ..."
+  print(t); if (shiny_running) { showNotification(t, type = "default") }
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "True Positives", Value = minimum.effect.count(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "True Positives", Value = minimum.effect.count(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "True Positives", Value = minimum.effect.count.signal(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "True Positives", Value = minimum.effect.count.signal(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "True Negatives", Value = true.negatives(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "True Negatives", Value = true.negatives(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df,data.frame(Measure = "False Positives", Value = typeI.error.rate(pb),
+                           `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "False Positives", Value = typeI.error.rate(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "False Negatives", Value = typeII.error.rate(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "False Negatives", Value = typeII.error.rate(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Signal Errors", Value = typeS.error.rate(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Signal Errors", Value = typeS.error.rate(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Exaggeration Factor", Value = exaggeration.factor(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Exaggeration Factor", Value = exaggeration.factor(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Signal Errors (Effects > Min)", Value = typeS.error.rate.above.min(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Signal Errors (Effects > Min)", Value = typeS.error.rate.above.min(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Exaggeration Factor (Effects > Min)", Value = exaggeration.factor.above.min(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Exaggeration Factor (Effects > Min)", Value = exaggeration.factor.above.min(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Positive Predictive Value", Value = pos.pred.value(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Positive Predictive Value", Value = pos.pred.value(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Positive Predictive Value", Value = pos.pred.value.signal(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Positive Predictive Value", Value = pos.pred.value.signal(pb),
+                            `Published Effects Only?` = pb))
+  
+  pb = T
+  df = rbind(df, data.frame(Measure = "Negative Predictive Value", Value = neg.pred.value(pb),
+                            `Published Effects Only?` = pb))
+  pb = F
+  df = rbind(df, data.frame(Measure = "Negative Predictive Value", Value = neg.pred.value(pb),
+                            `Published Effects Only?` = pb))
+  
+  if (input$calc.repro) {
+    
+    df$variable = character(nrow(df))
+    
+    t = "Reproducibility ..."
+    print(t); if (shiny_running) { showNotification(t, type = "default") }
+    
+    # Run RR many times and store the results
+    make.reps = function (rep_i) {
+      
+      print(paste(rep_i, "/", input$repro.repeats, sep = ""))
+      
+      rdf = data.frame(
+        Measure = character(0),
+        Value = numeric(0),
+        Sample.Size = numeric(0),
+        Rep = numeric(0)
+      )
+      
+      master.rep.df <<- perform.replications(input, rep.power = 0.95)
+      
+      # Computes reproducibility rates
+      rdf = rbind(rdf, reproducibility.rate(master.rep.df, "BRI", input, n.sample = 20, measure.name = "Reproducibility Rate (BRI)", rep_i))
+      rdf = rbind(rdf, reproducibility.rate(master.rep.df, "BRI", input, n.sample = -1, measure.name = "Reproducibility Rate (BRI) All Papers", rep_i))
+      rdf = rbind(rdf, reproducibility.rate(master.rep.df, "SSS", input, n.sample = 20, measure.name = "Reproducibility Rate (SSS)", rep_i))
+      rdf = rbind(rdf, reproducibility.rate(master.rep.df, "SSS", input, n.sample = -1, measure.name = "Reproducibility Rate (SSS) All Papers", rep_i))
+      # rdf = rbind(rdf, reproducibility.rate(master.rep.df, "ST", input, n.sample = 20, measure.name = "Reproducibility Rate (ST)", rep_i))
+      # rdf = rbind(rdf, reproducibility.rate(master.rep.df, "ST", input, n.sample = -1, measure.name = "Reproducibility Rate (ST) All Papers", rep_i))
+      
+      rdf
+    }
+    
+    rr.df = adply(1:input$repro.repeats, 1, make.reps)
+    
+    # CALCULATE CIs
+    rr.df = rr.df %>% group_by(Measure) %>% summarise(MeanValue = mean(Value, na.rm = T),
+                                                      SDValue = sd(Value, na.rm = T),
+                                                      SEMValue = SDValue / sqrt(input$repro.repeats),
+                                                      CI.side = -SEMValue * qnorm(0.05 / 2),
+                                                      CI.low = max(0, MeanValue - CI.side, na.rm = T),
+                                                      CI.high = min(MeanValue + CI.side, 1, na.rm = T),
+                                                      TypeIErrorRateMean = mean(TypeIErrorRate, na.rm = T),
+                                                      TypeIIErrorRateMean = mean(TypeIIErrorRate, na.rm = T),
+                                                      TypeSErrorRateMean = mean(TypeSErrorRate, na.rm = T),
+                                                      MFactorMean = mean(MFactor, na.rm = T),
+                                                      PPVMean = mean(PPV, na.rm = T),
+                                                      NPVMean = mean(NPV, na.rm = T)
+    )
+    
+    # ADD CIS TO RESULTs
+    rr.df = rr.df %>% select(Measure, MeanValue, CI.low, CI.high, TypeIErrorRateMean, TypeIIErrorRateMean, TypeSErrorRateMean, MFactorMean, PPVMean, NPVMean) %>% melt(id.vars = "Measure") %>% mutate(Published.Effects.Only. = T) %>% select(1,3,4,2)
+    colnames(rr.df) = colnames(df)
+    
+    df = rbind(df, rr.df)
+    
+  } else { # if not calc.repro, just add the variable column
+    df$variable = ""
+  }
+  
+  df
+}
+
+# TYPE I/FALSE POSITIVE ERROR RATE
+typeI.error.rate = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NA)
+  }
+  
+  # False Positives
+  ests$isError = (abs(ests$Real.Effect.Size) < ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  error.count = nrow(ests %>% filter(isError))
+  return (error.count / nrow(ests))
+}
+
+# TYPE II/FALSE NEGATIVE ERROR RATE
+typeII.error.rate = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # False Negatives
+  ests$isError = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value > ests$Alpha)
+  
+  error.count = nrow(ests %>% filter(isError))
+  return (error.count / nrow(ests))
+}
+
+# TYPE S/WRONG SIGNAL ERROR RATE
+typeS.error.rate = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value < Alpha)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Wrong Signal
+  ests$isError = (ests$Real.Effect.Size / ests$Estimated.Effect.Size < 0)
+  
+  error.count = nrow(ests %>% filter(isError))
+  return (error.count / nrow(ests))
+}
+
+
+# TYPE M/MAGNITUDE/EXAGGERATION FACTOR
+exaggeration.factor = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value < Alpha)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Magnitude Error
+  ests$Mfactor = ifelse(
+    ests$Real.Effect.Size / ests$Estimated.Effect.Size > 0,
+    ests$Estimated.Effect.Size / ests$Real.Effect.Size,
+    NA)
+  
+  Mfactor = median(ests$Mfactor, na.rm = T)
+  return (Mfactor)
+}
+
+# TYPE S/WRONG SIGNAL ERROR RATE FOR EFFECTS ABOVE MIN OF INTEREST
+typeS.error.rate.above.min = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value < Alpha, abs(Estimated.Effect.Size) >= Min.Interesting.Effect)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Wrong Signal
+  ests$isError = (ests$Real.Effect.Size / ests$Estimated.Effect.Size < 0)
+  
+  error.count = nrow(ests %>% filter(isError))
+  return (error.count / nrow(ests))
+}
+
+# TYPE M/MAGNITUDE/EXAGGERATION FACTOR FOR EFFECTS ABOVE MIN OF INTEREST
+exaggeration.factor.above.min = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value < Alpha, abs(Real.Effect.Size) >= Min.Interesting.Effect)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Magnitude Error
+  ests$Mfactor = ifelse(
+    ests$Real.Effect.Size / ests$Estimated.Effect.Size > 0,
+    ests$Estimated.Effect.Size / ests$Real.Effect.Size,
+    NA)
+  
+  Mfactor = median(ests$Mfactor, na.rm = T)
+  return (Mfactor)
+}
+
+# POSITIVE PREDICTIVE VALUE/1 - FALSE FINDING RATE
+pos.pred.value = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value <= Alpha)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Positive Predictive Value = True Positives / (True Positives + False Positives)
+  ests$isTP = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  tps = nrow(ests %>% filter(isTP))
+  return (tps / nrow(ests))
+}
+
+# POSITIVE PREDICTIVE VALUE/1 - FALSE FINDING RATE (CONDITIONAL ON SIGNAL BEING RIGHT)
+pos.pred.value.signal = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value <= Alpha)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Wrong Signal
+  ests$signalError = (ests$Real.Effect.Size / ests$Estimated.Effect.Size < 0)
+  
+  ests = ests %>% filter(!signalError)
+  
+  if (nrow(ests) == 0) {
+    return (NA)
+  }
+  
+  # Positive Predictive Value = True Positives / (True Positives + False Positives)
+  ests$isTP = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) & (ests$p.value <= ests$Alpha)
+  
+  tps = nrow(ests %>% filter(isTP))
+  return (tps / nrow(ests))
+}
+
+# NEGATIVE PREDICTIVE VALUE
+neg.pred.value = function(published.only = T) {
+  ests = estimates.df %>% filter(p.value > Alpha)
+  
+  if (published.only) {
+    ests = ests %>% filter(Published == T)
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Negative Predictive Value = True Negatives / (True Negatives + False Negatives)
+  ests$isTN = (abs(ests$Real.Effect.Size) < ests$Min.Interesting.Effect) &
+    (ests$p.value > ests$Alpha)
+  
+  tns = nrow(ests %>% filter(isTN))
+  return (tns / nrow(ests))
+}
+
+# TRUE POSITIVES
+minimum.effect.count = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # True positives
+  ests$isDiscovery = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  count = nrow(ests %>% filter(isDiscovery)) / nrow(ests)
+  return (count)
+}
+
+# TRUE POSITIVES (CONDITIONAL ON SIGNAL BEING RIGHT)
+minimum.effect.count.signal = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Wrong Signal
+  ests$signalError = (ests$Real.Effect.Size / ests$Estimated.Effect.Size < 0)
+  
+  ests = ests %>% filter(!signalError)
+  
+  if (nrow(ests) == 0) {
+    return (NA)
+  }
+  
+  # True positives
+  ests$isDiscovery = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  count = nrow(ests %>% filter(isDiscovery)) / nrow(ests)
+  return (count)
+}
+
+
+# TRUE NEGATIVES
+true.negatives = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # True negatives
+  ests$isNonDiscovery = (abs(ests$Real.Effect.Size) < ests$Min.Interesting.Effect) &
+    (ests$p.value > ests$Alpha)
+  
+  count = nrow(ests %>% filter(isNonDiscovery)) / nrow(ests)
+  return (count)
+}
+
+# N TRUE EFFECTS
+n.real.effects.above.minimum = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # Real effects above minimum of interest
+  ests$isDiscovery = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect)
+  
+  count = nrow(ests %>% filter(isDiscovery))
+  return (round(count,0))
+}
+
+# Number of true positives / total sample size
+efficiency = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # True positives above minimum of interest
+  ests$isDiscovery = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  count = nrow(ests %>% filter(isDiscovery))
+  resources = sum(estimates.df$Sample.Size)
+  
+  return (count / resources)
+}
+
+# Number of true positives / number of potential discoveries
+effectiveness = function(published.only = T) {
+  if (published.only) {
+    ests = estimates.df %>% filter(Published == T)
+  } else {
+    ests = estimates.df
+  }
+  
+  if (nrow(ests) == 0) {
+    return (NaN)
+  }
+  
+  # True positives above minimum of interest
+  ests$isDiscovery = (abs(ests$Real.Effect.Size) >= ests$Min.Interesting.Effect) &
+    (ests$p.value <= ests$Alpha)
+  
+  count = nrow(ests %>% filter(isDiscovery))
+  tried = n.real.effects.above.minimum(published.only)
+  
+  return (count / tried)
+}
+### EVALUATION FUNCTIONS WITHIN THE BLOCK ABOVE
---
+++ rep.r
@@ -0,0 +1,179 @@
+# Returns a data frame with the results of replications
+perform.replications = function(input, rep.power, n.reps = 3, positive.only = T) {
+  # rep.power = -1 means "use the same sample size as the original"
+  
+  # Filters the published estimates
+  rep.ests = estimates.df[Published == T]
+  
+  if (positive.only) {
+    rep.ests = rep.ests[p.value < Alpha]
+  }
+  
+  rep.ests = rep.ests[!duplicated(Effect.Index)]
+  
+  # Calculate N for the desired power according to the original estimate (not the real effect)
+  if (rep.power > 0) {
+    calc.n = function (eff, sd, wanted.pwr, alpha) {
+      pw = tryCatch(
+        { power.t.test(delta = eff, sd = sd, sig.level = alpha, power = wanted.pwr)$n },
+        error = function(e) { 2 # Error means N < 2, i.e. very large effect compared to the SE
+        })
+      return (ceiling(pw))
+    }
+    rep.ests$rep.sample.size = mapply(calc.n, rep.ests$Estimated.Effect.Size, rep.ests$Estimated.Pooled.SD, rep.power, input$alpha.threshold)
+  } else {
+    rep.ests$rep.sample.size = input$typical.sample.size
+  }
+  
+  # Receives an effect index, and performs an experiment, adding it to the rep.df
+  replicate.exp = function(effect.index, rep.df) {
+    if (shiny_running) {
+      rep.input = reactiveValuesToList(input)
+    } else {
+      rep.input = input
+    }
+    rep.input$typical.sample.size = rep.ests[Effect.Index == effect.index, rep.sample.size]
+    xp = perform.experiment(effect.index, rep.input, -1)
+  }
+  
+  rep.ests$Original.Effect.Size = rep.ests$Estimated.Effect.Size
+  rep.ests$Original.p.value = rep.ests$p.value
+  
+  rep.df = lapply(rep(rep.ests$Effect.Index, n.reps), replicate.exp, rep.df)
+  rep.df = rbindlist(rep.df)
+  rep.df = merge(rep.df, rep.ests[,.(Effect.Index, rep.sample.size, Original.Effect.Size, Original.p.value)], by = "Effect.Index")
+  
+  return (rep.df)
+}
+
+# Computes many types of reproducibility measures from a set of replications
+reproducibility.rate = function(master.rep.df, type, input, n.sample = -1, measure.name = "", rep_i = 0) {
+  
+  # type is one of:
+  # "BRI" (Brazilian Reproducibility Initiative)
+  # "SSS" (significant, in the same sense)
+  # "ST" (Small Telescopes)
+  # n.sample is the number of experiments selected to evaluate (-1 means all experiments)
+  
+  n.experiments = ifelse(n.sample > 0, n.sample, nrow(master.rep.df)/3)
+  
+  # Pick the sample of experiments: sample is alawys picked in order of the effect index, to assure that every measure will be calculated on the same sample
+  if (length(unique(master.rep.df$Effect.Index)) < n.experiments) {
+    cat("Not enough experiments to reproduce!")
+    return (-1)
+  }
+  
+  # eff.sample = sort(unique(master.rep.df$Effect.Index))[1:n.experiments]
+  eff.sample = sample(x = unique(master.rep.df$Effect.Index), size = n.experiments, replace = F)
+  rep.df = master.rep.df[Effect.Index %in% eff.sample]
+  rep.ests = estimates.df[Effect.Index %in% eff.sample]
+  
+  rep.ests = merge(rep.ests, rep.df %>% select("Effect.Index","rep.sample.size"), by = "Effect.Index")
+  rep.ests = rep.ests[!duplicated(Effect.Index)]
+  
+  
+  # Depending on the type of initiative, calculate the reproducibility rate
+  
+  # If BRI, do a meta analysis of the three replications and extract a prediction interval
+  if (type == "BRI") {
+    rep.df = rep.df[, run.ma(MeanControl, SDControl, Sample.Size,
+                             MeanTreated, SDTreated, Sample.Size,
+                             what = "PI"), Effect.Index]
+    
+    # rep.ests = rep.ests[!duplicated(Effect.Index)]
+    
+    rep.ests = merge(rep.ests, rep.df, by = "Effect.Index") %>%
+      select(Effect.Index, Real.Effect.Size, Estimated.Effect.Size, PI.lower, PI.upper)
+    rep.ests$Reproduced = rep.ests$Estimated.Effect.Size < rep.ests$PI.upper &
+      rep.ests$Estimated.Effect.Size > rep.ests$PI.lower
+    
+    
+    # If SSS, check if it is in the same sense of the original, and significant
+  } else if (type == "SSS") {
+    rep.res = rep.df[, run.ma(MeanControl, SDControl, Sample.Size,
+                              MeanTreated, SDTreated, Sample.Size,
+                              what = "S"), Effect.Index]
+    
+    rep.ests = merge(rep.ests, rep.res, by = "Effect.Index") %>%
+      select(Effect.Index, Real.Effect.Size, Estimated.Effect.Size, eff, signif)
+    rep.ests$Reproduced = rep.ests$signif &
+      rep.ests$eff / rep.ests$Estimated.Effect.Size > 0
+    
+    
+    # If ST check if it is significantly different from a small effect (defined as an effect that the original study has 33% power to detect) in a one sided T-test at 5% level
+    # The Small Telescopes approach recommends using a replication sample that is always 2.5 times the original sample size, as this gives about 80% power to  reject a "small effect"
+  } else if (type == "ST") {
+    
+    #returns power calculation results for t-test at 33% power using original estimates 
+    small_telescope_d33 = mapply(pwr.t.test, n = rep.ests$Sample.Size, power = 1/3)
+    small_telescope_d33 = as.data.frame(small_telescope_d33)
+    #transposes dataframe to correct rows vs columns
+    small_telescope_d33 = as.data.frame(t(small_telescope_d33))
+    #adds d33 to rep.ests
+    rep.ests$small_telescope_d33 = t(as.data.frame(small_telescope_d33$d))
+    
+    rep.res = rep.df[, run.ma(MeanControl, SDControl, Sample.Size,
+                              MeanTreated, SDTreated, Sample.Size,
+                              what = "S"), Effect.Index]
+    
+    rep.ests = merge(rep.ests, rep.res, by = "Effect.Index") %>%
+      select(Effect.Index, Real.Effect.Size, Estimated.Effect.Size, eff, se, rep.sample.size, signif, zval, small_telescope_d33)
+    
+    rep.ests$Reproduced = rep.ests$eff / rep.ests$Estimated.Effect.Size > 0 &
+      pt(abs(rep.ests$zval), df = rep.ests$rep.sample.size - 1, ncp = rep.ests$small_telescope_d33 * sqrt(rep.ests$rep.sample.size / 2)) > 0.05
+    rep.ests$small_telescope_d33 = NULL # GAMBIARRA, VER COM O PEDRO DEPOIS O QUE HOUVE ... PARECE QUE A COLUNA d_33 É UM DATA FRAME? ...
+  }
+  
+  tried.true = rep.ests[abs(Real.Effect.Size) >= input$min.effect.of.interest, .N]
+  tried.false = rep.ests[abs(Real.Effect.Size) < input$min.effect.of.interest, .N]
+  FP = rep.ests[Reproduced == T & abs(Real.Effect.Size) < input$min.effect.of.interest, .N]
+  FN = rep.ests[Reproduced == F & abs(Real.Effect.Size) >= input$min.effect.of.interest, .N]
+  TP = rep.ests[Reproduced == T & abs(Real.Effect.Size) >= input$min.effect.of.interest, .N]
+  TN = rep.ests[Reproduced == F & abs(Real.Effect.Size) < input$min.effect.of.interest, .N]
+  
+  reproduced = rep.ests[Reproduced == T, .N]
+  
+  rep.ests$Mfactor = ifelse(
+    rep.ests$Real.Effect.Size / rep.ests$Estimated.Effect.Size > 0,
+    rep.ests$Estimated.Effect.Size / rep.ests$Real.Effect.Size, NA)
+  # filter for abs(Estimated.Effect.Size) >= Min.Interesting.Effect?
+  
+  # browser()
+  
+  return (
+    data.frame(Measure = measure.name,
+               Value = reproduced / n.experiments,
+               TypeIErrorRate = FP / tried.false,
+               TypeIIErrorRate = FN / tried.true,
+               MFactor = mean(rep.ests$Mfactor, na.rm = T),
+               TypeSErrorRate = mean(rep.ests$Real.Effect.Size / rep.ests$Estimated.Effect.Size < 0, na.rm = T),
+               PPV = TP / (TP + FP),
+               NPV = TN / (TN + FN),
+               # Accuracy = ,
+               `Published Effects Only?` = T,
+               Sample.Size = n.sample,
+               Rep = rep_i)
+  )
+}
+
+# Runs a meta analysis given the means, SDs and Ns, then returns what is asked.
+run.ma = function(mean_control, sd_control, n_control, mean_treated, sd_treated, n_treated, what) {
+  
+  ess = escalc(measure = "MD", m1i = as.numeric(mean_treated), 
+               m2i = as.numeric(mean_control), sd1i = as.numeric(sd_treated), 
+               sd2i = as.numeric(sd_control), n1i = as.numeric(n_treated), 
+               n2i = as.numeric(n_control))
+  tryCatch({
+    m = rma(yi=yi, vi=vi, data = ess, measure = "MD", method = "REML", control=list(maxiter=1000))
+    pred = predict.rma(m, level = 0.95, digits = 3)
+    PI = list(PI.lower = pred$cr.lb, PI.upper = pred$cr.ub, beta = m$beta[[1]])
+    signif = list(eff = pred$pred,  se = pred$se, zval = m$zval, signif = abs(pred$pred) - 1.96 * pred$se > 0)
+    beta = list(beta = m$beta[[1]])
+  }, error = function(e) {
+    PI = list(PI.lower = -999, PI.upper = 999)
+    signif = list(eff = 999, signif = FALSE)
+    beta = list(beta = 999)
+  })
+  
+  if (what == "PI") { PI } else if (what == "S") { signif } else if (what == "EST") { beta } else if (what == "RMA") { m }
+}
