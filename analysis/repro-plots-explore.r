3ef38aaf2f7e02fd1973331d656b764adbc86989
KleberNeves <kleber.na@gmail.com>
2021-04-07 12:17:43 -0300

    Implement RMSE plot and add option for xlabel

--- analysis/repro-plots-explore.r
+++ analysis/repro-plots-explore.r
@@ -46,14 +46,15 @@ theme_set(figtheme)
 # Additionally, you can pass a filter to the functions, to be applied before
 # All these setting are registered in the caption of the figure and in the filename
 
-filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate_prev = F) {
+filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate_prev = F, which_agg_prev) {
   # Filter the data if a filter is given
   if (!rlang::quo_is_null(to_include)) {
     D = D %>% filter(rlang::eval_tidy(to_include, D))
   }
   
   # Filter the types to plot
-  if (to_plot == "reproducibility") { to_plot_suffix = "ReproRate" }
+  if (to_plot %in% c("reproducibility", "root mean square error"))
+  { to_plot_suffix = "ReproRate" }
   else if (aggregate_prev) {
     if (to_plot == "sensitivity") { to_plot_suffix = c("TP", "FN") }
     else if (to_plot == "specificity") { to_plot_suffix = c("TN", "FP") }
@@ -65,12 +66,12 @@ filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate
   to_plot_cols = paste0(sort(rep(types, length(to_plot_suffix))), "_", to_plot_suffix)
   
   keep = c(to_plot_cols, facetting, x)
-  if (aggregate_prev) keep = c(keep, "Prev_Lit")
+  if (aggregate_prev) keep = c(keep, which_agg_prev)
   
   D = D %>% select(all_of(keep))
   
-  if (aggregate_prev) {
-    agg_cols = c(facetting, x, "Prev_Lit")
+  if (aggregate_prev & to_plot != "root mean square error") {
+    agg_cols = c(facetting, x, which_agg_prev)
     D = D %>% group_by(across(all_of(agg_cols))) %>%
       summarise(across(all_of(to_plot_cols), sum)) %>%
       pivot_longer(cols = -all_of(agg_cols)) %>%
@@ -86,34 +87,56 @@ filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate
     } else if (to_plot == "specificity") {
       D = D %>% mutate(value = TN / (TN + FP))
     }
+  } else if (aggregate_prev & to_plot == "root mean square error") {
+    D$AggPrev = D[[which_agg_prev]]
+    agg_cols = c(facetting, x, "AggPrev")
+    D = D %>%
+      mutate(across(all_of(to_plot_cols), ~ (.x - AggPrev) ^ 2, .names = "{col}_SqErr")) %>%
+      group_by(across(all_of(agg_cols))) %>%
+      summarise(across(all_of(to_plot_cols), ~ sqrt(mean(.x, na.rm = T)), .names = "{col}")) %>%
+      pivot_longer(cols = -all_of(agg_cols)) %>%
+      mutate(type = name %>% str_remove_all("_ReproRate")) %>%
+      select(-name)
+    colnames(D)[colnames(D) == "AggPrev"] = which_agg_prev
   }
-  
+
   D
 }
 
-general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL, show_points = F, aggregate_prev = F) {
+general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL, show_points = F, aggregate_prev = F, which_agg_prev = "Prev_Lit", xlabel = NULL) {
   
   # Prepare tidy dataset for ggplot
-  D = filtered_data(D, x, to_plot, facetting, types, to_include, aggregate_prev)
+  D = filtered_data(D, x, to_plot, facetting, types, to_include, aggregate_prev, which_agg_prev)
   D$xvar = D[[x]]
   
   # Build the plot
   facet_formula = as.formula(paste(facetting[1], "~", facetting[2]))
+  if (is.null(xlabel)) {
+    xlabel = x
+  }
   
   p = ggplot(D) +
     aes(x = xvar, y = value, group = type, color = type) +
     geom_line(stat = "summary", size = 0.65) +
-    scale_y_continuous(limits = c(0,1)) +
     facet_grid(facet_formula) +
     labs(
       color = "",
-      x = x,
-      y = to_plot %>% str_to_title(),
-      title = to_plot %>% str_to_title()
+      x = xlabel,
+      y = to_plot %>% str_to_sentence(),
+      title = to_plot %>% str_to_sentence()
     )
   
-  if (is.numeric(D$xvar))
-    p = p + scale_x_continuous(limits = c(0,1))
+  if (max(D$value <= 1))
+    p = p + scale_y_continuous(limits = c(0,1))
+  else
+    p = p + scale_y_continuous(limits = c(0,NA))
+  
+  if (is.numeric(D$xvar)) {
+    if (max(D$value <= 1))
+      p = p + scale_x_continuous(limits = c(0,1))
+    else
+      p = p + scale_x_continuous(limits = c(0,NA))
+  }
   
   if (show_points)
     p = p + geom_point(size = 1.5)
@@ -129,12 +152,12 @@ general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL,
 # Lines represent the different measures of reproducibility (you can specify a subset)
 # Facetted in grid by two of the parameters
 
-tracking_plot = function (D, to_plot, prev, facetting, types, to_include = NULL, show_points = F) {
+tracking_plot = function (D, to_plot, prev, facetting, types, to_include = NULL, show_points = F, xlabel = NULL) {
   
   if (prev == "sample") x = "Prev_Sample"
   else if (prev == "literature") x = "Prev_Lit"
 
-  general_rep_plot(D, x, to_plot, facetting, types, enquo(to_include), show_points)
+  general_rep_plot(D, x, to_plot, facetting, types, enquo(to_include), show_points, xlabel = xlabel)
 }
 
 ### Aggregate tracking plot
@@ -144,9 +167,9 @@ tracking_plot = function (D, to_plot, prev, facetting, types, to_include = NULL,
 # Facetted in grid by two of the parameters (except prevalence)
 # Data is aggregated for all prevalences available
 
-aggregate_plot = function (D, x, to_plot, facetting, types, to_include = NULL, show_points = F) {
+aggregate_plot = function (D, x, to_plot, facetting, types, to_include = NULL, show_points = F, xlabel = NULL) {
   
-  general_rep_plot(D, x, to_plot, facetting, types, enquo(to_include), show_points, aggregate_prev = T)
+  general_rep_plot(D, x, to_plot, facetting, types, enquo(to_include), show_points, aggregate_prev = T, which_agg_prev = "Prev_Lit", xlabel = xlabel)
  
 }
 
@@ -158,10 +181,14 @@ aggregate_plot = function (D, x, to_plot, facetting, types, to_include = NULL, s
 # Bar or lollipop plot
 # Facetted in grid by two of the parameters
 
-rmse_plot = function () {
+rmse_plot = function (D, x, prev, facetting, types, to_include = NULL, show_points = F, xlabel = NULL) {
+  
+  general_rep_plot(D, x, "root mean square error", facetting, types, enquo(to_include), show_points, aggregate_prev = T, which_agg_prev = prev, xlabel = xlabel)
   
 }
 
+types_to_plot = global_rep_types[c(1,2,9,12)]
+rmse_plot(repdata, x = "interlab.label", prev = "Prev_Lit", facetting = c("bias.label", "power.label"), types = types_to_plot, to_include = (N == 10))
 
 ##### Test plots #####
 
