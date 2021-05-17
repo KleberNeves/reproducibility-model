a01bee73a6b6627eeb845fdbc42dec2142bdcc27
KleberNeves <kleber.na@gmail.com>
2021-05-17 18:14:33 -0300

    Add function to plot specification curve

--- analysis/repro-plots-explore.r
+++ analysis/repro-plots-explore.r
@@ -11,6 +11,7 @@
 # All these setting are registered in the caption of the figure and in the filename
 
 filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate_prev = F, which_agg_prev) {
+  # browser()
   # Filter the data if a filter is given
   if (!rlang::quo_is_null(to_include)) {
     D = D %>% filter(rlang::eval_tidy(to_include, D))
@@ -44,7 +45,8 @@ filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate
         measure = name %>% str_extract("(_TP|_TN|_FP|_FN)") %>% str_remove_all("_")
       ) %>%
       select(-name) %>%
-      pivot_wider(id_cols = c(type, all_of(agg_cols)), names_from = measure, values_from = value)
+      pivot_wider(id_cols = c(type, all_of(agg_cols)),
+                  names_from = measure, values_from = value)
     
     if (to_plot == "sensitivity") {
       D = D %>% mutate(value = TP / (TP + FN))
@@ -62,19 +64,26 @@ filtered_data = function (D, x, to_plot, facetting, types, to_include, aggregate
       mutate(type = name %>% str_remove_all("_ReproRate")) %>%
       select(-name)
     colnames(D)[colnames(D) == "AggPrev"] = which_agg_prev
+  } else if (!aggregate_prev) {
+    D = D %>% pivot_longer(cols = all_of(to_plot_cols), names_to = "type")
   }
 
   D
 }
 
 general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL, show_points = F, aggregate_prev = F, which_agg_prev = "Prev_Lit", xlabel = NULL) {
-  
+  # browser()
   # Prepare tidy dataset for ggplot
   D = filtered_data(D, x, to_plot, facetting, types, to_include, aggregate_prev, which_agg_prev)
   D$xvar = D[[x]]
   
   # Build the plot
-  facet_formula = as.formula(paste(facetting[1], "~", facetting[2]))
+  if (length(facetting) == 1) {
+    facet_formula = as.formula(paste("~", facetting[1]))
+  } else if (length(facetting) == 2) {
+    facet_formula = as.formula(paste(facetting[1], "~", facetting[2]))  
+  }
+  
   if (is.null(xlabel)) {
     xlabel = x
   }
@@ -82,7 +91,6 @@ general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL,
   p = ggplot(D) +
     aes(x = xvar, y = value, group = type, color = type) +
     geom_line(stat = "summary", size = 0.65) +
-    facet_grid(facet_formula) +
     labs(
       color = "",
       x = xlabel,
@@ -90,13 +98,19 @@ general_rep_plot = function (D, x, to_plot, facetting, types, to_include = NULL,
       title = to_plot %>% str_to_sentence()
     )
   
-  if (max(D$value <= 1))
+  if (length(facetting) == 1) {
+    p = p + facet_wrap(facet_formula)
+  } else if (length(facetting) == 2) {
+    p = p + facet_grid(facet_formula)
+  }
+  
+  if (max(D$value <= 1, na.rm = T))
     p = p + scale_y_continuous(limits = c(0,1))
   else
     p = p + scale_y_continuous(limits = c(0,NA))
   
   if (is.numeric(D$xvar)) {
-    if (max(D$value <= 1))
+    if (max(D$xvar <= 1, na.rm = T))
       p = p + scale_x_continuous(limits = c(0,1))
     else
       p = p + scale_x_continuous(limits = c(0,NA))
@@ -147,4 +161,71 @@ rmse_plot = function (D, x, prev, facetting, types, to_include = NULL, show_poin
   
   general_rep_plot(D, x, "root mean square error", facetting, types, enquo(to_include), show_points, aggregate_prev = T, which_agg_prev = prev, xlabel = xlabel)
   
+}
+
+plot_specification_curve = function (repro_rate_to_plot, spec_parameters, repro_rate_to_plot2 = NULL) {
+  spec_param_values = map(spec_parameters, ~unique(DF[[.x]])) %>% unlist()
+  
+  DF_WIDE = bind_cols(
+    map(1:length(spec_parameters), function (i) {
+      par_col = spec_parameters[i]
+      values = unique(DF[[par_col]])
+      bind_cols(
+        map(values, function (x) {
+          df = data.frame(value = DF[[par_col]] == x)
+          colnames(df) = x
+          df
+        })
+      )
+    })
+  )
+  
+  DF = bind_cols(list(DF, DF_WIDE))
+  # browser()
+  DF$repro_rate = DF[[repro_rate_to_plot]]
+  if (!is.null(repro_rate_to_plot2)) {
+    DF$repro_rate2 = DF[[repro_rate_to_plot2]]
+    DF_REPRO = DF %>% select(index, repro_rate, repro_rate2) %>%
+      pivot_longer(cols = -index)
+    plot_repro = ggplot(DF_REPRO) +
+      aes(x = reorder(index, value), y = value, color = name) +
+      theme(legend.position = "top")
+  } else {
+    plot_repro = ggplot(DF) +
+      aes(x = reorder(index, repro_rate), y = repro_rate)
+  }
+  
+  plot_repro = plot_repro +
+    geom_point(size = 0.5) +
+    labs(x = "", y = "Reproducibility\nRate") +
+    ylim(c(0,1)) +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          panel.grid = element_blank(), plot.title = element_blank())
+  
+  if (!is.null(repro_rate_to_plot2)) {
+    cols_to_keep = c("index","repro_rate","repro_rate2", spec_param_values)
+    SPECIFICATION = DF %>%
+      select(all_of(cols_to_keep)) %>%
+      pivot_longer(cols = -c(index, repro_rate, repro_rate2), names_to = "param")
+  } else {
+    cols_to_keep = c("index","repro_rate", spec_param_values)
+    SPECIFICATION = DF %>%
+      select(all_of(cols_to_keep)) %>%
+      pivot_longer(cols = -c(index, repro_rate), names_to = "param")
+  }
+  
+  plot_spec2 = ggplot(SPECIFICATION) +
+    aes(x = reorder(index, repro_rate),
+        y = param, fill = param, alpha = as.numeric(value)) +
+    geom_tile(height = 0.4) +
+    scale_alpha(range = c(0,1)) +
+    # scale_fill_manual(breaks = spec_param_values, values = spec_colors) +
+    labs(x = "", y = "") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          legend.position = "none", panel.grid = element_blank(),
+          axis.text.y = element_text(size = 11), plot.title = element_blank())
+  
+  plot_grid(plot_repro, plot_spec2, align = "v", axis = "l", ncol = 1, nrow = 2, rel_heights = c(1,2))
 }
